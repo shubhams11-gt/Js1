5e3d1481b832137dbd16a4f6566833d3cf5b2af8
shubham <shs23010@gmail.com>
2022-11-08 19:28:15 +0530

    commit

--- app.js
+++
@@ -1,9 +0,0 @@
-let menuIcon = document.querySelectorAll(".icon");
-menuIcon.forEach((element, index) => {
-  element.addEventListener("click", () => {
-    document.querySelector(".active").classList.remove("active");
-    element.classList.add("active");
-    const root = document.querySelector(':root');
-    root.style.setProperty('--distance', `${70.5 * index}px`);
-  });
-});
